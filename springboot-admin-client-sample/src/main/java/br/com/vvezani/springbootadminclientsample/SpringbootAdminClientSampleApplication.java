816ab4ce12afff72286cc8c72e4d85c31d8c2e72
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-02-01 00:47:05 +0100

    First commit

---
+++ springboot-admin-client-sample/src/main/java/br/com/vvezani/springbootadminclientsample/SpringbootAdminClientSampleApplication.java
@@ -0,0 +1,14 @@
+package br.com.vvezani.springbootadminclientsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminClientSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminClientSampleApplication.class, args);
+	}
+
+}
+
---
+++ springboot-admin-server-sample/src/main/java/br/com/vvezani/springbootadminserversample/SpringbootAdminServerSampleApplication.java
@@ -0,0 +1,14 @@
+package br.com.vvezani.springbootadminserversample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAdminServerSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAdminServerSampleApplication.class, args);
+	}
+
+}
+
