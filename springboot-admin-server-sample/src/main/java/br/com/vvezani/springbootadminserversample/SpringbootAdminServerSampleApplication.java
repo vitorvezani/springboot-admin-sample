9f391aaf26bd02c5144cc47f9e6d8e5fce570a41
Vitor Rodrigo Vezani <vitor.vezani@freshcells.de>
2019-02-01 01:07:20 +0100

    Fix client project

--- springboot-admin-server-sample/src/main/java/br/com/vvezani/springbootadminserversample/SpringbootAdminServerSampleApplication.java
+++ springboot-admin-server-sample/src/main/java/br/com/vvezani/springbootadminserversample/SpringbootAdminServerSampleApplication.java
@@ -1,14 +1,16 @@
 package br.com.vvezani.springbootadminserversample;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class SpringbootAdminServerSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootAdminServerSampleApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootAdminServerSampleApplication.class, args);
+  }
 
 }
 
